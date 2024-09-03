e56074fdbaebfc9568123f201965b1488ecbc6ae
1harshitasingh <harshita253746@gmail.com>
2024-09-03 12:09:12 +0530

    valid program

--- src/com/harshita/lecture1/Lecture1.java
+++ src/com/harshita/lecture1/Lecture1.java
@@ -1,4 +1,18 @@
 package com.harshita.lecture1;
 
+import java.util.Scanner;
+
 public class Lecture1 {
+public static void main(String []args){
+    Scanner sc=new Scanner (System.in);
+            int button = sc.nextInt();
+            if(button==1)
+
+    {
+        System.out.println("Hello");
+    }  else if (button==2)
+    { System.out.println("Namaste");
+
+    }
+    }
 }
