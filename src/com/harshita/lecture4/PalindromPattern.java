aa85f0ea83ffb995867214de31a9aa6d6bcb59ed
1harshitasingh <harshita253746@gmail.com>
2024-09-05 18:08:20 +0530

    Function

--- src/com/harshita/lecture4/PalindromPattern.java
+++ src/com/harshita/lecture4/PalindromPattern.java
@@ -1,4 +1,28 @@
 package com.harshita.lecture4;
 
 public class PalindromPattern {
+    public static void main(String args[]) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            //spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+
+            //first part
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+
+            //second part
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
+
+
