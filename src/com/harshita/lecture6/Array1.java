86796ee31fae6d5dd541f4e11e48c18093dd361e
1harshitasingh <harshita253746@gmail.com>
2024-09-06 15:46:13 +0530

    Array

---
+++ src/com/harshita/lecture6/Array1.java
@@ -0,0 +1,14 @@
+package com.harshita.lecture6;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 85;
+        marks[1] = 95;
+        marks[2] = 90;
+      for(int i=0; i<3; i++){
+      System.out.println(marks[i]);
+
+    }
+}
+}
