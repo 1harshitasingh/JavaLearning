1939e433d3b8389d7f92e3ce5faa60e37ff02969
1harshitasingh <harshita253746@gmail.com>
2024-09-08 19:16:01 +0530

    append

--- src/com/harshita/lecture9/StringBuilder3.java
+++ src/com/harshita/lecture9/StringBuilder3.java
@@ -4,12 +4,12 @@ public class StringBuilder3 {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("tony");
         System.out.println(sb);
-        sb.insert(2,'n');
+        sb.insert(2, 'n');
         System.out.println(sb);
 
         // delete the extra n
 
-        sb.delete(2,3);
+        sb.delete(2, 3);
         System.out.println(sb);
     }
 }
---
+++ src/com/harshita/lecture9/StringBuilder4.java
@@ -0,0 +1,12 @@
+package com.harshita.lecture9;
+
+public class StringBuilder4 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+}
