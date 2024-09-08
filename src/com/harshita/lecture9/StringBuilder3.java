721dd8b93257d9f3ee7d77c3bfc5732cc978c37e
1harshitasingh <harshita253746@gmail.com>
2024-09-08 19:02:15 +0530

    delete character

---
+++ src/com/harshita/lecture9/StringBuilder3.java
@@ -0,0 +1,17 @@
+package com.harshita.lecture9;
+
+public class StringBuilder3 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("tony");
+        System.out.println(sb);
+        sb.insert(2,'n');
+        System.out.println(sb);
+
+        // delete the extra n
+
+        sb.delete(2,3);
+        System.out.println(sb);
+    }
+}
+
+
