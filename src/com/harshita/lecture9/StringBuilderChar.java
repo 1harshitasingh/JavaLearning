850fcb35f66905a5ab2357c14ff8303546b4df06
1harshitasingh <harshita253746@gmail.com>
2024-09-08 18:42:26 +0530

    char at index

---
+++ src/com/harshita/lecture9/StringBuilderChar.java
@@ -0,0 +1,10 @@
+package com.harshita.lecture9;
+
+public class StringBuilderChar {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Harshita");
+        System.out.println(sb);
+        // char at index 0
+        System.out.println(sb.charAt(0));
+    }
+}
