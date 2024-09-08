8d9f8f115b9b13cbcf59d74366e590cb2c8e4ee3
1harshitasingh <harshita253746@gmail.com>
2024-09-08 19:21:25 +0530

    character length

---
+++ src/com/harshita/lecture9/StringBuilder5.java
@@ -0,0 +1,13 @@
+package com.harshita.lecture9;
+
+public class StringBuilder5 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb.length());
+    }
+}
+
