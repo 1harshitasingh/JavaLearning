b6d5302193768ba7c8e782ac83aa57fd74e2c595
1harshitasingh <harshita253746@gmail.com>
2024-09-08 18:56:49 +0530

    insert character at given index

---
+++ src/com/harshita/lecture9/StringBuilder2.java
@@ -0,0 +1,12 @@
+package com.harshita.lecture9;
+
+public class StringBuilder2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("tony");
+        System.out.println(sb);
+       sb.insert(2,'n');
+       System.out.println(sb);
+    }
+}
+
+
