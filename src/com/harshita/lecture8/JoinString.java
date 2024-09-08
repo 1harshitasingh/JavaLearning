5d3aeda44830e3690ee6413c39ee914d52e34df1
1harshitasingh <harshita253746@gmail.com>
2024-09-08 12:55:42 +0530

    join two string

---
+++ src/com/harshita/lecture8/JoinString.java
@@ -0,0 +1,14 @@
+package com.harshita.lecture8;
+
+
+
+public class JoinString {
+public static void main(String[]args){
+
+    String firstName ="Harshita";
+    String lastName ="Singh";
+    String fullName = firstName+lastName;
+    System.out.println(fullName);
+
+}
+}
