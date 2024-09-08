ed9a02cc3ba84c9aced6206f28c248eb61b24cb5
1harshitasingh <harshita253746@gmail.com>
2024-09-08 13:02:38 +0530

    length of string

---
+++ src/com/harshita/lecture8/StringLength.java
@@ -0,0 +1,13 @@
+package com.harshita.lecture8;
+
+public class StringLength {
+    public static void main(String[] args) {
+
+        String firstName = "Harshita";
+        String lastName = "Singh";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName.length());
+
+    }
+}
+
