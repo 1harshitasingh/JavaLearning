af46742bb6684a4f2df63c16a2745d476e52d7cf
1harshitasingh <harshita253746@gmail.com>
2024-09-08 13:50:34 +0530

    sub string

--- src/com/harshita/lecture8/SubStrings.java
+++ src/com/harshita/lecture8/SubStrings.java
@@ -1,4 +1,9 @@
 package com.harshita.lecture8;
 
 public class SubStrings {
+    public static void main(String[]args){
+        String sentence= "My name is Harshita Singh";
+        String name=sentence.substring(11,sentence.length());
+        System.out.println(name);
+    }
 }
