a1a0659554e76e6004d9e4db68f04c5ac95da23d
1harshitasingh <harshita253746@gmail.com>
2024-09-09 17:38:46 +0530

    set bit

---
+++ src/com/harshita/lecture10/SetBits.java
@@ -0,0 +1,18 @@
+package com.harshita.lecture10;
+
+import java.util.*;
+
+public class SetBits {
+
+    public static void main(String args[]) {
+        int n = 5; //0101
+        int pos = 1;
+        int bitMask = 1 << pos;
+
+
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+    }
+}
+
+
