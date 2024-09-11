bcd13c1eb3dde077bd3d112b8ea9dce076cca266
1harshitasingh <harshita253746@gmail.com>
2024-09-11 19:04:19 +0530

    add element in between

---
+++ src/com/harshita/lecture13/ArrayLists2.java
@@ -0,0 +1,32 @@
+package com.harshita.lecture13;
+
+
+import java.util.ArrayList;
+
+class ArrayLists2 {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<String> list2 = new ArrayList<String>();
+        ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+
+        //add elements
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+
+
+        //to get an element
+        int element = list.get(0); // 0 is the index
+        System.out.println(element);
+
+//add element in between
+
+
+        list.add(1, 2); // 1 is the index and 2 is the element to be added
+
+
+        System.out.println(list);
+    }
+}
