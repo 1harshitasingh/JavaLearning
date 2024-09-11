b1d593791cc56a42192183351c8ee3123d17cb51
1harshitasingh <harshita253746@gmail.com>
2024-09-11 18:47:54 +0530

    add element in arraylist

---
+++ src/com/harshita/lecture13/Arraylists.java
@@ -0,0 +1,21 @@
+package com.harshita.lecture13;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+class ArrayLists {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<String> list2 = new ArrayList<String>();
+        ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+
+        //add elements
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+
+    }
+}
