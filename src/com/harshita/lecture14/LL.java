08c30c5141f9ecf392ab68102e6d4679642cbdcf
1harshitasingh <harshita253746@gmail.com>
2024-09-12 16:41:38 +0530

    Linked list

--- src/com/harshita/lecture14/LL.java
+++ src/com/harshita/lecture14/LL.java
@@ -11,13 +11,13 @@ class LL {
         list.add("a");
         list.addLast("list");
         list.addFirst("this");
-        list.add(3, "linked");
+        list.add(4, "linked");
         System.out.println(list);
 
 
         System.out.println(list.get(0));
         System.out.println(list.size());
-        list.remove(3);
+        list.remove(4);
         list.removeFirst();
         list.removeLast();
 
