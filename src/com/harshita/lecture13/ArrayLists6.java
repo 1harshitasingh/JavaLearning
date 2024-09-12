1292dd8f2ae1eeb26deb2f5af1171938d120b878
1harshitasingh <harshita253746@gmail.com>
2024-09-12 09:52:50 +0530

    loops on ArrayLists

--- src/com/harshita/lecture13/ArrayLists6.java
+++ src/com/harshita/lecture13/ArrayLists6.java
@@ -10,9 +10,9 @@ class ArrayLists6 {
 
         //add elements
         list.add(1);
-        list.add(3);
+        list.add(8);
         list.add(4);
-        list.add(5);
+        list.add(9);
         System.out.println(list);
 
 
